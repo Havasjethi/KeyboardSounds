d35cc563b833e787ab647098a05f5a228650c7bd
Havasjethi <havasjethi@gmail.com>
2023-03-06 22:28:18 +0100

    Cli message for starting, update input path

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use serde_json::Value;
 use std::collections::HashMap;
 use std::time::Instant;
 
+mod util_functions;
+
 use kira::{
     manager::{backend::cpal::CpalBackend, AudioManager, AudioManagerSettings},
     sound::static_sound::{StaticSoundData, StaticSoundSettings},
@@ -42,8 +44,7 @@ impl PluginHandledKeyPressHandler {
     fn new<'a>(plugin_path: &'a str) -> Self {
         use std::fs;
         use std::path;
-        let plugin_base = path::Path::new("plugins");
-        let plugin_base = plugin_base.join(plugin_path);
+        let plugin_base = path::Path::new(plugin_path);
 
         let plugin_config = plugin_base.join("config.json");
 
@@ -85,105 +86,25 @@ impl PluginHandledKeyPressHandler {
 
 impl KeyPressHandler for PluginHandledKeyPressHandler {
     fn handle(&mut self, key: rdev::Key) -> () {
-        use rdev::Key;
-        // Source: https://github.com/hainguyents13/mechvibes/blob/master/src/libs/keycodes.js#L99P
-        let key_code = match key {
-            // Key::F13 => "91",
-            // Key::F14 => "92",
-            // Key::F15 => "93",
-            Key::F1 => "59",
-            Key::F2 => "60",
-            Key::F3 => "61",
-            Key::F4 => "62",
-            Key::F5 => "63",
-            Key::F6 => "64",
-            Key::F7 => "65",
-            Key::F8 => "66",
-            Key::F9 => "67",
-            Key::F10 => "68",
-            Key::F11 => "87",
-            Key::F12 => "88",
-            Key::BackQuote => "`",
-            Key::Num1 => "2",
-            Key::Num2 => "3",
-            Key::Num3 => "4",
-            Key::Num4 => "5",
-            Key::Num5 => "6",
-            Key::Num6 => "7",
-            Key::Num7 => "8",
-            Key::Num8 => "9",
-            Key::Num9 => "10",
-            Key::Num0 => "11",
-            Key::Minus => "12",
-            Key::Equal => "13",
-            Key::Backspace => "14",
-            Key::Tab => "15",
-            Key::CapsLock => "58",
-            Key::KeyA => "30",
-            Key::KeyB => "48",
-            Key::KeyC => "46",
-            Key::KeyD => "32",
-            Key::KeyE => "18",
-            Key::KeyF => "33",
-            Key::KeyG => "34",
-            Key::KeyH => "35",
-            Key::KeyI => "23",
-            Key::KeyJ => "36",
-            Key::KeyK => "37",
-            Key::KeyL => "38",
-            Key::KeyM => "50",
-            Key::KeyN => "49",
-            Key::KeyO => "24",
-            Key::KeyP => "25",
-            Key::KeyQ => "16",
-            Key::KeyR => "19",
-            Key::KeyS => "31",
-            Key::KeyT => "20",
-            Key::KeyU => "22",
-            Key::KeyV => "47",
-            Key::KeyW => "17",
-            Key::KeyX => "45",
-            Key::KeyY => "21",
-            Key::KeyZ => "44",
-            Key::LeftBracket => "26",
-            Key::RightBracket => "27",
-            Key::BackSlash => "43",
-            Key::SemiColon => "39",
-            Key::Quote => "40",
-            Key::Comma => "51",
-            Key::Dot => "52",
-            Key::Slash => "53",
-            Key::Escape => "28",
-            Key::Space => "57",
-            Key::PrintScreen => "3639",
-            Key::ScrollLock => "70",
-            Key::Pause => "3653",
-            Key::Insert => "3666",
-            Key::Delete => "3667",
-            Key::End => "3663",
-            Key::PageUp => "3657",
-            Key::PageDown => "3665",
-            Key::UpArrow => "57416",
-            Key::DownArrow => "57419",
-            Key::RightArrow => "57421",
-            Key::LeftArrow => "57424",
-            Key::ShiftLeft => "42",
-            Key::ShiftRight => "54",
-            Key::Alt => "56",
-            Key::AltGr => "3675",
-            Key::MetaLeft => "3676",
-            Key::MetaRight => "3677",
-            Key::Unknown(135) => "48",
-            Key::NumLock => "69",
-            _ => "1",
-        };
+        let key_code = util_functions::keyToKeyCode(key);
         let valami: StaticSoundData = self.sounds.get(key_code).unwrap().clone();
         self.manager.play(valami).unwrap();
     }
 }
 
 fn main() -> () {
-    let x = PluginHandledKeyPressHandler::new("custom-sexy-voice");
-    // let x = PluginHandledKeyPressHandler::new("kalih-box-white");
+    let vars = std::env::args().collect::<Vec<String>>();
+
+    if vars.len() < 2 {
+        println!("Havas-KeyLogger 0.1.0");
+        println!("Usage: rust_sound <confuration path>");
+        println!("\n\t!! Provide configuration folder path as argument !!\n");
+        println!("U can browse sound packs here: https://docs.google.com/spreadsheets/d/1PimUN_Qn3CWqfn-93YdVW8OWy8nzpz3w3me41S8S494");
+        return;
+    }
+
+    let config_folder = vars.get(1).unwrap();
+    let x = PluginHandledKeyPressHandler::new(config_folder);
+
     init_listener(Box::new(x));
 }
